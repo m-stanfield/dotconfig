4fc6698821ed6900e5052d8d1c16db16d2be867e
m-stanfield <mstan44@gmail.com>
2023-11-12 22:05:07 -0500

    updating lsp and adding neo-tree

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -176,6 +176,16 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
+  {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+      }
+  },
 -- {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
 --   {
 --     "rcarriga/nvim-notify",
@@ -191,7 +201,7 @@ require('lazy').setup({
 --     vim.api.nvim_win_set_config(win, { zindex = 100 })
 --   end,
 -- }
-  },
+--  },
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
