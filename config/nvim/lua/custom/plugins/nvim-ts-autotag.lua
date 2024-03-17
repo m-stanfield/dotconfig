8516313eb85f419095d0a288de2b859817e8003a
m-stanfield <mstan44@gmail.com>
2024-03-16 20:34:50 -0400

    adding autotag to lua

---
+++ config/nvim/lua/custom/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,16 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  "windwp/nvim-ts-autotag",
+
+  config = function()
+    require 'nvim-treesitter.configs'.setup {
+      autotag = {
+        enable = true,
+      }
+    }
+  end,
+
+}
