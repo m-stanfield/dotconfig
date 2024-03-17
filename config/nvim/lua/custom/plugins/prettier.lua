7da26783d7dac53cf62380f79c150c975a644c95
m-stanfield <mstan44@gmail.com>
2024-03-16 20:52:33 -0400

    adding prettier

---
+++ config/nvim/lua/custom/plugins/prettier.lua
@@ -0,0 +1,30 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  "MunifTanjim/prettier.nvim",
+
+  confgg = function()
+    local prettier = require("prettier")
+
+    prettier.setup({
+      bin = 'prettier', -- or `'prettierd'` (v0.23.3+)
+      filetypes = {
+        "css",
+        "graphql",
+        "html",
+        "javascript",
+        "javascriptreact",
+        "json",
+        "less",
+        "markdown",
+        "scss",
+        "typescript",
+        "typescriptreact",
+        "yaml",
+      },
+    })
+  end,
+
+}
