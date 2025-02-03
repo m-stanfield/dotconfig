46f533d586bf689a32cc9614eb5d863ded8ecbeb
m-stanfield <mstan44@gmail.com>
2025-02-02 21:20:21 -0500

    adding commented out copilot

---
+++ config/nvim/lua/custom/plugins/copilot.lua
@@ -0,0 +1,10 @@
+return {
+    --    "github/copilot.vim",
+    --    dependencies = {
+    --    },
+    --    config = function()
+    --        local opts = { desc = "Toggle Copilot" }
+    --       vim.api.nvim_set_keymap("n", "<Leader>bc", "<cmd>ToggleCopilot<cr>", opts)
+    --    end,
+}
+
