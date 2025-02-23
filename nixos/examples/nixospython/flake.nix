3f344f3caaa17d1bdcd52fefaf6b63cdcf4b40bc
m-stanfield <mattstanfield52@gmail.com>
2025-02-23 14:16:05 -0500

    updating flake to work with python and numpy

--- nixos/examples/nixospython/flake.nix
+++ nixos/examples/nixospython/flake.nix
@@ -1,17 +1,46 @@
+
+
 {
-  description = "Python development environment";
+  description = "Python development environment with Nix";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-  outputs = { self, nixpkgs }: {
-    devShells.default = with import nixpkgs { system = "x86_64-linux"; };
-      mkShell {
-        buildInputs = [
-          python311
-          python311Packages.pip
-          python311Packages.virtualenv
-        ];
-      };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        python = pkgs.python311;
+        pythonPackages = python.withPackages (ps: with ps; [
+          requests
+        ]);
+
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "python-dev-shell";
+
+          # Provide Python and dependencies
+          packages = [ python pythonPackages pkgs.virtualenv ];
+          env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+          pkgs.stdenv.cc.cc.lib
+          pkgs.libz
+          ];
+
+          shellHook = ''
+            if [ ! -d ".venv" ]; then
+              echo "Creating virtual environment in ./.venv..."
+              ${python}/bin/python -m venv .venv
+              source .venv/bin/activate
+              pip install --upgrade pip
+            fi
+            source .venv/bin/activate
+            echo "Virtual environment activated!"
+          '';
+        };
+      }
+    );
 }
 
