00fdbca51ecc973b36c13b9576e8a62aa158ae8b
m-stanfield <mstan44@gmail.com>
2025-02-22 19:22:47 -0500

    updating tmux profile to match preexisting tmux.conf

--- nixos/home-manager/tmux.nix
+++ nixos/home-manager/tmux.nix
@@ -8,15 +8,37 @@
       resurrect
       continuum
       catppuccin
+      vim-tmux-navigator
+      yank
     ];
     extraConfig = ''
-      set -g @plugin 'tmux-plugins/sensible'
-      set -g @plugin 'tmux-plugins/resurrect'
-      set -g @plugin 'tmux-plugins/continuum'
+      set -g @plugin 'tmux-plugins/tmux-sensible'
       set -g @plugin 'catppuccin/tmux'
+      set -g @plugin 'christoomey/vim-tmux-navigator'
+      set -g @plugin 'tmux-plugins/tmux-yank'
 
-      # Set Catppuccin Mocha theme
-      set -g @catppuccin_flavour 'mocha'
+      set -sg escape-time 10
+
+      set -g @catppuccin_window_left_separator ""
+      set -g @catppuccin_window_right_separator " "
+      set -g @catppuccin_window_middle_separator " █"
+      set -g @catppuccin_window_number_position "right"
+
+      set -g @catppuccin_window_default_fill "number"
+      set -g @catppuccin_window_default_text "#W"
+
+      set -g @catppuccin_window_current_fill "number"
+      set -g @catppuccin_window_current_text "#W"
+
+      set -g @catppuccin_status_modules "directory user host session"
+      set -g @catppuccin_status_left_separator  " "
+      set -g @catppuccin_status_right_separator ""
+      set -g @catppuccin_status_right_separator_inverse "no"
+      set -g @catppuccin_status_fill "icon"
+      set -g @catppuccin_status_connect_separator "no"
+
+      set -g @catppuccin_directory_text "#{pane_current_path}"
+      set -ga terminal-overrides ",xterm-256color:Tc"
     '';
   };
 }
