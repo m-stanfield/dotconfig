cd87a83f27e871fbd74ead9515a59915165c023e
m-stanfield <mattstanfield52@gmail.com>
2025-03-02 21:38:28 -0500

    adding some i3 keybindings

--- nixos/home-manager/i3.nix
+++ nixos/home-manager/i3.nix
@@ -1,5 +1,8 @@
 { config, lib, pkgs, ... }:
 
+let
+  mod = "Mod4";
+in 
 {
 
 #in home.nix
@@ -7,7 +10,25 @@
     enable = true;
     package = pkgs.i3-gaps;
     config = {
-      modifier = "Mod4";
+      fonts = [
+        "DejaVu Sans Mono:size=10"
+        "FontAwesome:size=10"
+        "JetBrains Mono:size=10"
+      ];
+      keybindings = lib.mkOptionDefault{
+        # Focus
+        "${mod}+h" = "focus left";
+        "${mod}+j" = "focus down";
+        "${mod}+k" = "focus up";
+        "${mod}+l" = "focus right";
+
+        # Move
+        "${mod}+Shift+h" = "move left";
+        "${mod}+Shift+j" = "move down";
+        "${mod}+Shift+k" = "move up";
+        "${mod}+Shift+l" = "move right";
+      };
+      modifier = mod;
       gaps = {
         inner = 10;
         outer = 5;
