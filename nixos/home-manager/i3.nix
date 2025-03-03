7982b65c77f65cc3da5e6bc83c5fae6ce05ef039
m-stanfield <mattstanfield52@gmail.com>
2025-03-03 18:50:00 -0500

    adding i3 settings

--- nixos/home-manager/i3.nix
+++ nixos/home-manager/i3.nix
@@ -1,21 +1,29 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs,home, ... }:
 
 let
   mod = "Mod4";
 in 
 {
+  home.packages = with pkgs; [
+    i3status
+    i3lock
+    i3-gaps
+    rofi 
+  ];
 
 #in home.nix
   xsession.windowManager.i3 = {
     enable = true;
-    package = pkgs.i3-gaps;
     config = {
+      terminal = "kitty";
       fonts = [
         "DejaVu Sans Mono:size=10"
         "FontAwesome:size=10"
         "JetBrains Mono:size=10"
       ];
       keybindings = lib.mkOptionDefault{
+        "${mod}+Return" = "exec kitty"; # Replace Mod1 with Mod4 if using Super key
+        "${mod}+space" = "exec rofi -show run"; # Replace Mod1 with Mod4 if using Super key
         # Focus
         "${mod}+h" = "focus left";
         "${mod}+j" = "focus down";
