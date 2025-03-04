ca43f46981b3848658b2856d49020f02097e521d
m-stanfield <mattstanfield52@gmail.com>
2025-03-03 23:11:50 -0500

    adding backupFileExtension option

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -101,7 +101,6 @@
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
-
   # Install firefox.
   programs.firefox.enable = true;
   programs.zsh.enable = true;
@@ -131,6 +130,7 @@
 
   home-manager = {
     extraSpecialArgs = {inherit inputs; };
+  backupFileExtension = "backup";
     users = {
       "matt" = import ./home-manager/home.nix;
     };
