258605badb9f6684656bf6c9463e07a13ac30e78
m-stanfield <mattstanfield52@gmail.com>
2025-03-09 14:34:56 -0400

    adding spotify and discord

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -114,6 +114,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    spotify
+    discord
     stow
     cloc
     htop
