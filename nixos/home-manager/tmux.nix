bf6d26ef7d694f91f126e6640cdf88311cb5becd
m-stanfield <mattstanfield52@gmail.com>
2025-05-04 17:51:30 -0400

    not working

--- nixos/home-manager/tmux.nix
+++ nixos/home-manager/tmux.nix
@@ -1,45 +1,39 @@
-
-{ config, pkgs, ... }:
-{
+{ pkgs, ... }: {
   programs.tmux = {
     enable = true;
+    baseIndex = 1;
+    terminal = "tmux-256color";
+    mouse = true;
+    plugins = with pkgs; [
+      tmuxPlugins.vim-tmux-navigator
+      {
+      plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavor 'mocha'
+          set -g @catppuccin_window_left_separator ""
+          set -g @catppuccin_window_right_separator " "
+          set -g @catppuccin_window_middle_separator " █"
+          set -g @catppuccin_window_number_position "right"
 
-    plugins = [
-      pkgs.tmuxPlugins.better-mouse-mode
-      (pkgs.tmuxPlugins.catppuccin.overrideAttrs (_: {
-        src = pkgs.fetchFromGitHub {
-          owner = "catppuccin";
-          repo = "tmux";
-         rev= "073ee54992c59fedcc29c1525a26f95691f0ae1f";
-        sha256= "11gmvifq6lir48rar1n1zp085ss6x3h52aawjqyrhh1i1zlxzpdd";
-        };
-      }))
-      pkgs.tmuxPlugins.sensible
-      pkgs.tmuxPlugins.vim-tmux-navigator
+          set -g @catppuccin_window_default_fill "number"
+          set -g @catppuccin_window_default_text "#W"
+
+          set -g @catppuccin_window_current_fill "number"
+          set -g @catppuccin_window_current_text "#W"
+
+          set -g @catppuccin_status_modules "directory user host session"
+          set -g @catppuccin_status_left_separator  " "
+          set -g @catppuccin_status_right_separator ""
+          set -g @catppuccin_status_right_separator_inverse "no"
+          set -g @catppuccin_status_fill "icon"
+          set -g @catppuccin_status_connect_separator "no"
+
+          set -g @catppuccin_directory_text "#{pane_current_path}"
+              '';
+      }
     ];
-
     extraConfig = ''
-      set -g @catppuccin_window_left_separator ""
-      set -g @catppuccin_window_right_separator " "
-      set -g @catppuccin_window_middle_separator " █"
-      set -g @catppuccin_window_number_position "right"
-
-      set -g @catppuccin_window_default_fill "number"
-      set -g @catppuccin_window_default_text "#W"
-
-      set -g @catppuccin_window_current_fill "number"
-      set -g @catppuccin_window_current_text "#W"
-
-      set -g @catppuccin_status_modules "directory user host session"
-      set -g @catppuccin_status_left_separator  " "
-      set -g @catppuccin_status_right_separator ""
-      set -g @catppuccin_status_right_separator_inverse "no"
-      set -g @catppuccin_status_fill "icon"
-      set -g @catppuccin_status_connect_separator "no"
-
-      set -g @catppuccin_directory_text "#{pane_current_path}"
-      set -ga terminal-overrides ",xterm-256color:Tc"
+      set -ag terminal-overrides ",$TERM:RGB"
     '';
   };
 }
-
