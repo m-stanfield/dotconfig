5eb50aba193db35c15e4abd43dc330c5245b8c8b
m-stanfield <mattstanfield52@gmail.com>
2025-05-04 23:18:49 -0400

    adding systemctl-tui

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -115,6 +115,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    systemctl-tui
     feh
     playerctl 
     unityhub
