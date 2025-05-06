fec6461b564223b3070c39bf8d838260ebc286b3
m-stanfield <mattstanfield52@gmail.com>
2025-05-05 22:46:44 -0400

    swapping to i3status-rust

--- nixos/home-manager/i3.nix
+++ nixos/home-manager/i3.nix
@@ -1,12 +1,17 @@
 { config, lib, pkgs,home, ... }:
 {
   home.packages = with pkgs; [
-    i3status
+    i3status-rust
     i3lock
     i3-gaps
+
   ];
   home.activation.linkI3Config = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     ln -sfn "$HOME/code/dotconfig/config/i3/config" "$HOME/.config/i3/config"
+  '';
+
+  home.activation.linkI3StatusRustConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    ln -sfn "$HOME/code/dotconfig/config/i3status-rust/config.toml" "$HOME/.config/i3status-rust/config.toml"
   '';
 
   home.activation.linkI3StatusConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
