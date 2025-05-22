ca56ee69a11ccdbf471bff09aec798e717dbae77
m-stanfield <mattstanfield52@gmail.com>
2025-05-21 20:35:51 -0400

    adding ffxiv and glances

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -115,6 +115,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    xivlauncher
+    glances
     obsidian
     pulseaudio
     maim
