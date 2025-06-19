9b025ab240344754f268f7f0d48e9612403cb468
m-stanfield <mattstanfield52@gmail.com>
2025-06-19 12:04:42 -0400

    adding telegram

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -115,6 +115,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    telegram-desktop
     qalculate-qt
     kalker
     speedcrunch
