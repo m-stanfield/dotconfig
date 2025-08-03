25e91340eda092aa9d2590a24e982983040691c4
m-stanfield <mattstanfield52@gmail.com>
2025-08-03 14:29:52 -0400

    opening port to test website on phone

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -197,7 +197,7 @@
   };
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 3000 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
