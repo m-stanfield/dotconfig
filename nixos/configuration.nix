222d47befd9d1a14e1d872c3538659f7db71aef6
m-stanfield <mattstanfield52@gmail.com>
2025-09-06 13:15:16 -0400

    adding starship for terminal formatting

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -114,6 +114,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    starship
     slack
     filezilla
     (retroarch.withCores (cores: with cores; [
