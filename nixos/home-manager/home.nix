7b56abf0008f1cc1a33713df847f91b1a254b1a9
m-stanfield <mattstanfield52@gmail.com>
2025-10-25 20:02:20 -0400

    adding basic hyprland and broke into different file

--- nixos/home-manager/home.nix
+++ nixos/home-manager/home.nix
@@ -5,6 +5,7 @@
     ./tmux.nix
     ./neovim.nix
     ./synology.nix
+    ./hyprland.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
@@ -19,29 +20,6 @@
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
-  wayland.windowManager.hyprland.enable = true;
-
-
-  wayland.windowManager.hyprland.settings = {
-     "$mod" = "SUPER";
-     bind =
-       [
-         "$mod, F, exec, firefox"
-         ", Print, exec, grimblast copy area"
-       ]
-       ++ (
-         # workspaces
-         # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-         builtins.concatLists (builtins.genList (i:
-             let ws = i + 1;
-             in [
-               "$mod, code:1${toString i}, workspace, ${toString ws}"
-               "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-             ]
-           )
-           9)
-       );
-   };
 
 
 
