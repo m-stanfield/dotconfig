71b48ac1ad91c4997e106e7f817c35d4b81e2289
m-stanfield <mattstanfield52@gmail.com>
2025-12-04 20:57:39 -0500

    automatically mounting ssd on desktop

--- nixos/hosts/desktop/configuration.nix
+++ nixos/hosts/desktop/configuration.nix
@@ -14,5 +14,14 @@
   boot.loader.grub.device = "/dev/nvme1n1";
   boot.loader.grub.useOSProber = true;
 
+  systemd.tmpfiles.rules = [
+    # Type Path        Mode    UID     GID     Age  Argument
+    "d     /mnt/ssd    0755    1000    100     -    -"
+  ];
+  fileSystems."/mnt/ssd" =
+    { device = "/dev/disk/by-uuid/1e447dce-19ff-4a88-a30c-8586c8f4bee3";
+      fsType = "ext4";
+      options = [ "defaults" "noatime" ];
+    };
 
 }
