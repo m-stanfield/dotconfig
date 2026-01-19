261a06364ce9afc7f0b1b29e99a0f4bb3e60cba0
m-stanfield <mattstanfield52@gmail.com>
2026-01-19 14:42:41 -0500

    adding clipboard and extension pack to virtualbox

--- nixos/virtual-box.nix
+++ nixos/virtual-box.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }:
 {
   virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
   users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
+  virtualisation.virtualbox.guest.enable = true;
+  virtualisation.virtualbox.guest.clipboard = true;
 }
