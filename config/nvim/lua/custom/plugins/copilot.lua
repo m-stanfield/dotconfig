094084935772dfe2ddd704bccde337527f485665
m-stanfield <mattstanfield52@gmail.com>
2026-02-08 11:39:04 -0500

    removing copilot from plugsins

--- config/nvim/lua/custom/plugins/copilot.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'github/copilot.vim',
-  dependencies = {},
-  config = function()
-    local opts = { desc = 'Toggle Copilot' }
-    vim.api.nvim_set_keymap('n', '<Leader>bc', '<cmd>ToggleCopilot<cr>', opts)
-  end,
-}
