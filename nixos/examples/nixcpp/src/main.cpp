3fb665ded8bb8b3ed2b610adc50afa22548f7ddd
m-stanfield <mattstanfield52@gmail.com>
2026-08-01 17:05:46 -0400

    adding str example for inspection

--- nixos/examples/nixcpp/src/main.cpp
+++ nixos/examples/nixcpp/src/main.cpp
@@ -5,6 +5,7 @@
 int main(int argc, char **argv) {
   importtest::MessagePrinter printer("Default message");
   int value2 = 803;
+  std::vector<std::string> strvec = {"aabcd", "", "\n"};
   std::vector<int> vec = {1, 2, 3};
   printer.display();
   int e = printer.display("1.3 + 3.0 = " + std::to_string(value2));
@@ -12,6 +13,10 @@ int main(int argc, char **argv) {
   auto b = printer.display("1.3 + 3.0 = " + std::to_string(value));
   for (const auto &v : vec) {
     auto c = printer.display("vec element: " + std::to_string(v));
+  }
+
+  for (const auto &v : strvec) {
+    auto c = printer.display("vec element: " + v);
   }
   return 0;
 }
